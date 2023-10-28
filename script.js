c357dfbcfdcfd4f0dec36d5b3a87687bbbbdcd76
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-10-28 10:29:27 -0400

    Scripted for compatibility edits

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Add color (not func.)
+
+// Default Value
+document.getElementById("ta-html").textContent = "<!-- HTML -->";
+document.getElementById("ta-css").textContent = "/* CSS */";
+document.getElementById("ta-js").textContent = "// JavaScript";
 document.getElementById("ra").setAttribute('srcdoc', '<h2 style = "font-family: sans-serif;">Hello, world!</h2>');
+// Run Button
 document.getElementById("run").addEventListener('click', () => {  document.getElementById("ra").setAttribute(
   'srcdoc',
-    document.getElementById("ta-html").value + "<style>" + document.getElementById("ta-css").value + "</style><script>" + document.getElementById("ta-js").value + "</script>"
+    document.getElementById("ta-html").innerText + "<style>" + document.getElementById("ta-css").innerText + "</style><script>" + document.getElementById("ta-js").innerText + "</script>"
   );
 });
