fc8aa6bc392461c6cba2638eaecea8c295953bb3
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-10-28 15:51:03 -0400

    Updated script to cooperate

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Add color (not func.)
-
 // Default Value
-document.getElementById("ta-html").textContent = "<!-- HTML -->";
-document.getElementById("ta-css").textContent = "/* CSS */";
-document.getElementById("ta-js").textContent = "// JavaScript";
+document.getElementById("ta-html").textContent = "<h2>Hello, World!</h2>";
+document.getElementById("ta-css").innerHTML = "h2{<br />&nbsp;&nbsp;font-family: sans-serif;<br />}";
 document.getElementById("ra").setAttribute('srcdoc', '<h2 style = "font-family: sans-serif;">Hello, world!</h2>');
 // Run Button
 document.getElementById("run").addEventListener('click', () => {  document.getElementById("ra").setAttribute(
