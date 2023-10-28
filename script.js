49845c8513469184ae2eaea5365089ede80a0a7b
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-10-28 09:50:50 -0400

    Scripted run button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-document.getElementById("ra").setAttribute('srcdoc', '<h2 style = "font-family: sans-serif;">Hello, world!</h2>')
-document.body.onkeydown = function(event) {
-  document.getElementById("ra").setAttribute(
+document.getElementById("ra").setAttribute('srcdoc', '<h2 style = "font-family: sans-serif;">Hello, world!</h2>');
+document.getElementById("run").addEventListener('click', () => {  document.getElementById("ra").setAttribute(
   'srcdoc',
     document.getElementById("ta-html").value + "<style>" + document.getElementById("ta-css").value + "</style><script>" + document.getElementById("ta-js").value + "</script>"
   );
-};
+});
